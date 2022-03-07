9f68efd2f39a3eb90a496fa6ac64bbca43fbb6f4
OSMANLIOGLU <osmanlioglu@OSMANLIOGLU-MacBook-Air.local>
2022-03-07 12:21:09 +0300

    fix

--- src/app/main/services/instance.js
+++ src/app/main/services/instance.js
@@ -2,6 +2,9 @@ import axios from 'axios'
 
 const instance = axios.create({
   baseURL: process.env.REACT_APP_API,
+  headers: {
+    Auhorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJoYWthbiIsImlhdCI6MTY0NjY0NDQyMywiZXhwIjoxNjQ2NjUxNjIzfQ.MuMzhsInirL5-BNGI2K8kNq2Zq2gciZn3G5So4FrtAQ`
+  }
 })
 
 export default instance
